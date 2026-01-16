5ca9e97a476f09be06662a7c3fb81ff1a73f887d
elmoro <fahd.rafik@ensea.fr>
2026-01-16 18:11:35 +0100

    updates for prod

--- app.py
+++ app.py
@@ -851,5 +851,5 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5005))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    #port = int(os.environ.get('PORT', 5005))
+    #app.run(host='0.0.0.0', port=port, debug=False)
