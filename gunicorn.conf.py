4d60185dd268d4425b766f744b0392460980712c
elmoro <fahd.rafik@ensea.fr>
2026-01-17 10:10:54 +0100

    new gunicorn conf file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,13 @@
 import os
 
-# Bind port 24 given by render 
-
+# Internal interface 
+"""
+Internet → Render's Load Balancer (public IP:443/80) 
+          ↓
+          Render's Internal Routing
+          ↓
+          Your App (0.0.0.0:10000) ← Internal only
+"""
 bind = f"0.0.0.0:{os.environ.get('PORT', '1000')}"
 workers = 2
 worker_class = "sync"
