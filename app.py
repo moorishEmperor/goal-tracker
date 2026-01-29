e186b0fa7b2d300a4aa874ad9b80a598517ba681
elmoro <fahd.rafik@ensea.fr>
2026-01-29 21:54:36 +0100

    update app.py

--- app.py
+++ app.py
@@ -849,7 +849,10 @@ def init_db():
         db.create_all()
         logger.info("Database tables created successfully")
 
+if __name__ != '__main__':
+    init_db()
+
 if __name__ == '__main__':
     init_db()
-    #port = int(os.environ.get('PORT', 5005))
-    #app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
